007b36a5e42f4ccf7aa1accd1f640106357df973
Flyn Sequeira <flyns3694@gmail.com>
2019-06-13 16:06:11 +0530

    Basic build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {
       path: '',
-      redirectTo: '/home',
+      redirectTo: '/login',
       pathMatch: 'full',
   },
   {
       path: 'home',
       component: HomeComponent,
   },
+  {
+      path: 'login',
+      component: LoginComponent,
+  }
 ];
